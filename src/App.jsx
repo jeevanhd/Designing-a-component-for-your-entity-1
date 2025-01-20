929953dfdc944bc879be6153c1afcfaca1a41b9a
jeevanhd <jeevan.hd@kalvium.community>
2025-01-20 14:25:53 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Usercard from './components/usercard.jsx';
+import "./App.css";
+import UserCard from "./components/usercard.jsx";
 
 function App() {
   const cards = Array.from({ length: 10 });
   const gridStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(4, 1fr)',
-    gap: '16px',
-    padding: '16px',
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 1fr)",
+    gap: "16px",
+    padding: "16px",
   };
 
   return (
     <div style={gridStyle}>
       {cards.map((_, index) => (
-        <Usercard key={index} />
+        <UserCard key={index} />
       ))}
     </div>
   );
--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,33 @@
-// write the component code here
+import React, { useState } from "react";
+const styles = {};
+
+const userCard = () => {
+  const [data, setData] = useState({
+    name: "jeevan",
+    email: "iWontTellMyEmail@email.com",
+    phone: "93431 69420",
+    address: "some were on earth",
+    image: "secret.png",
+  });
+
+  return (
+    <div class="card">
+      <img
+        src={data.image}
+        alt="Profile Photo"
+        className="profile-photo"
+        style={styles.profilePhoto}
+      />
+
+      <h2 className="name">{data.name}</h2>
+
+      <p className="mail">{data.email}</p>
+
+      <p className="phone-number">{data.phone}</p>
+
+      <p className="address">{data.address}</p>
+    </div>
+  );
+};
+
+export default userCard;
